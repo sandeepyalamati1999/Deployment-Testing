d051eaffe7ab910f27ff06d2fb5d66f9ef5f394b
sandeep <sandeepyalamati1999@gmail.com>
2026-06-22 11:36:36 +0530

    helth check changes

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -7,8 +7,5 @@ const router = Router();
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 
-router.get('/health', (_req: any, res: { json: (arg0: { status: string; timestamp: string; }) => void; }) => {
-  res.json({ status: 'ok', timestamp: new Date().toISOString() });
-});
 
 export default router;
